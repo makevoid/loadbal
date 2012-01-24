0053125ece9d0dc28e8b7a8b772a0518f9e771d0
Francesco Canessa <makevoid@gmail.com>
2012-01-24 02:37:09 +0100

    first working version

---
+++ README.md
@@ -0,0 +1,10 @@
+# Loadbal
+
+### sets / unsets load balancing policy on router: TP-Link, model: TL-R470T+
+
+### usage:
+
+from shell:
+
+    loadbal activate
+    loadbal deactivate
---
+++ lib/loadbal.rb
@@ -0,0 +1,31 @@
+require 'mechanize'
+
+class Loadbal
+
+  # sets / unsets load balancing policy on router: TP-Link, model: TL-R470T+
+
+  HOST = "192.168.0.1"
+  USER = "admin"
+  PASS = File.read(File.expand_path("~/.password")).sub(/33/, '')
+  
+  def activation_url(active=true)
+    "http://#{HOST}/userRpm/WanBalancePolicyRpm.htm?IpConn=#{"2" unless active}&ipConnMaxAge=360&ipConnOblAge=600&appMaxAge=600&appOblAge=1200&Save=Save"
+  end
+
+  def initialize
+    @agent = Mechanize.new
+    @agent.basic_auth USER, PASS
+    @agent.get "http://#{HOST}"
+  end
+  
+  # activate load balancing policy
+  def activate
+    @agent.get activation_url 
+  end
+  
+  # deactivates load balancing
+  def deactivate
+    @agent.get activation_url(false)
+  end
+  
+end
