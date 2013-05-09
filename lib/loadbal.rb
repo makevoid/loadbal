048b2f9f9a5bd5f0d9899aedba757a4184cf2837
Francesco Canessa <makevoid@gmail.com>
2013-05-10 01:54:50 +0200

    basic auth update and more info on what is activate command, that even I forgot it :D

--- lib/loadbal.rb
+++ lib/loadbal.rb
@@ -7,25 +7,25 @@ class Loadbal
   HOST = "192.168.0.1"
   USER = "admin"
   PASS = File.read(File.expand_path("~/.password")).strip.sub(/33/, '')
-  
+
   def activation_url(active=true)
     "http://#{HOST}/userRpm/WanBalancePolicyRpm.htm?IpConn=#{"2" unless active}&ipConnMaxAge=360&ipConnOblAge=600&appMaxAge=600&appOblAge=1200&Save=Save"
   end
 
   def initialize
     @agent = Mechanize.new
-    @agent.add_auth "http://#{HOST}",  USER, PASS
+    @agent.basic_auth USER, PASS
     @agent.get "http://#{HOST}"
   end
-  
+
   # activate load balancing policy
   def activate
-    @agent.get activation_url 
+    @agent.get activation_url
   end
-  
+
   # deactivates load balancing
   def deactivate
     @agent.get activation_url(false)
   end
-  
+
 end
